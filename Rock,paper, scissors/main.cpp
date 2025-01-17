ab57ce4ffb68da4ff42e3ec3cd6388bfbd3e250a
Tanner Bebb <17tbebb@gmail.com>
2025-01-17 14:09:31 -0800

    Player setup?

--- Rock,paper, scissors/main.cpp
+++ Rock,paper, scissors/main.cpp
@@ -13,15 +13,23 @@ paper > rock
 using namespace std;
 
 int main() {
+	//This will store the user's selected opitions.
+	string playerChoice;
+
 	//String array that has the three opitions. This will be what the CPU player will choose from. 
 	string options[3] = { "rock", "papper", "scissors" };
 
 	//This is the command that is setting the seeding to 0.
 	srand(time(0));
 
+	//This is using the rand function to create a random index number from the size of the string array.
 	int randomIndex = rand() % options->size();
 
-	string randomString = options[randomIndex];
+	//This will select a string from the list of opitions using the randomly generated index.
+	string CPUChoice = options[randomIndex];
 
-	cout << randomString;
+	//This is asking the user for their choice.
+	cout << "Please choose one of the following: Rock, Papper, Scissors" << endl;
+	cin >> playerChoice;
+
 }
