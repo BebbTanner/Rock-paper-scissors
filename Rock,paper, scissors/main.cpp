48028afeda0fbb145a501a85b3367a5c6b1d707c
Tanner Bebb <17tbebb@gmail.com>
2025-01-17 13:54:59 -0800

    I set up my string array.

--- Rock,paper, scissors/main.cpp
+++ Rock,paper, scissors/main.cpp
@@ -1,6 +1,20 @@
+/*
+Rock paper scissors game.
+
+rock > scissors
+scissors > paper
+paper > rock
+
+*/
+
 #include <iostream>
+#include <ctime> //time based seeding
+#include <cstdlib> //rand and srand
 using namespace std;
 
 int main() {
+	//String array that has the three opitions. This will be what the CPU player will choose from. 
+	string options[3] = { "rock", "papper", "scissors" };
+
 
 }
