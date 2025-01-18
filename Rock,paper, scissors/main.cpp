7f3a155a473b9c04d1b9f83e00ac08828e4a0e02
Tanner Bebb <17tbebb@gmail.com>
2025-01-18 12:34:09 -0800

    Started fresh on this.

--- Rock,paper, scissors/main.cpp
+++ Rock,paper, scissors/main.cpp
@@ -13,36 +13,6 @@ paper > rock
 using namespace std;
 
 int main() {
-	string playerChoice;
-	string options[3] = { "rock", "paper", "scissors" };
 
-	//This is the command that is setting the seeding to 0.
-	srand(time(0));
-	//This is using the rand function to create a random index number from the size of the string array.
-	int randomIndex = rand() % options->size();
-
-
-	//This will select a string from the list of opitions using the randomly generated index.
-	string CPUChoice = options[randomIndex];
-
-	//Something is happening, sometimes the cpuchoice prints nothing and I am not sure as to why.
-	cout << CPUChoice << endl;
-
-	//This is asking the user for their choice.
-	cout << "Please choose one of the following: Rock, Papper, Scissors" << endl;
-	cin >> playerChoice;
-	cout << endl;
-
-	//This will throw an error to the user letting them know that they have not selected a proper option.
-	if (playerChoice != "rock" || playerChoice != "Rock") {
-		cout << 1 << endl;
-	}
-	else if (playerChoice != "paper" || playerChoice != "Paper") {
-		cout << 2 << endl;
-	}
-	else if (playerChoice != "scissors" || playerChoice != "Scissors") {
-		cout << 3 << endl;
-	}
-	
 
 }
